61df25e71ee04ff1a5c8dbd45521ceaaa5215a63
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2026-01-11 21:42:55 +0100

    Deploy Lambda from S3 bucket with versioning fetch

--- lib/compute-construct.ts
+++ lib/compute-construct.ts
@@ -1,7 +1,6 @@
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as s3 from 'aws-cdk-lib/aws-s3'
-// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
 import * as ssm from 'aws-cdk-lib/aws-ssm'
 import { Construct } from 'constructs';
 
@@ -16,19 +15,16 @@ export class ComputeConstruct extends Construct {
   constructor(scope: Construct, id: string, props: ComputeConstructProps) {
     super(scope, id);
 
-    /* eslint-disable capitalized-comments */
-    // const objectVersion = ssm.StringParameter.valueForStringParameter(
-    //   this,
-    //   '/glotticaorg/api/lambda-version'
-    // );
+    const objectVersion = ssm.StringParameter.valueForStringParameter(
+      this,
+      '/glotticaorg/api/lambda-version'
+    );
 
     this.apiLambda = new lambda.Function(this, 'ApiLambda', {
       architecture: lambda.Architecture.ARM_64,
-      code: lambda.Code.fromInline('export const handler = async () => ({ statusCode: 200, body: \'\' });'),
-      // code: lambda.Code.fromBucketV2(props.codeBucket, 'apiLambdaCode/handler.zip', {
-      //  objectVersion,
-      // }),
-      /* eslint-enable capitalized-comments */
+      code: lambda.Code.fromBucketV2(props.codeBucket, 'apiLambdaCode/handler.zip', {
+        objectVersion,
+      }),
       environment: {
         TABLE_NAME: props.table.tableName,
       },
